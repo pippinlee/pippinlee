d18c89569d70725c0c493f1d00932f4aa6df6049
Pippin Lee <pippinblee@gmail.com>
2016-10-02 14:11:53 -0400

    update

--- js/work/data.js
+++ js/work/data.js
@@ -5,7 +5,7 @@ var work = {
     'item-link': 'parade-sf.com',
     'item-description': "I led development alongside the designers at Parade to help build a new website for their design firm. Craft CMS gives a powerful architecture, while allowing us to build all UI's from scratch. Optimizing animations on the homepage allowed us to really bring the site alive.",
     'tech': ['Craft CMS, MAMP stack', 'CSS and jQuery for animation', 'SASS and boostrap grid system that allowed us to build 3 customized styles for different screen sizes', 'Asset optimization: SVG, device, specific load'],
-    'images': ['../../img/work/parade-2.png'],
+    'images': ['../img/work/parade-2.png'],
     'color': '#4b3fb5',
     'shadow1': 'rgba(36, 58, 214, 0.5)',
     'shadow2': 'rgba(44, 32, 234, 0.22)'
@@ -16,9 +16,9 @@ var work = {
     'item-link': 'riverdaleathletics.ca',
     'item-description': 'Built and designed Riverdale Athletics over a few days, with the goal of having a simplified one-page landing with clean info and actions. Specifically built with the understanding that the vast majority of use will come from mobile viewports.',
     'tech': ['jekyll', 'jQuery', 'CSS', 'grid system: boostrap'],
-    'images': ['../../img/work/rac-promo.png'],
+    'images': ['../img/work/rac-promo.png'],
     'color': '#ffd509',
-    'shadow1': '../../rgba(152, 148, 26, 0.5)',
+    'shadow1': '../rgba(152, 148, 26, 0.5)',
     'shadow2': 'rgba(234, 228, 32, 0.15)'
   },
   'koala': {
@@ -27,7 +27,7 @@ var work = {
     'item-link': 'koalatesters.com',
     'item-description': 'Koala Testers was built alongside the Heist Design team to quickly prototype a product that would help both internal and external design firms with their design process. User feedback is critical to the design process, this tool aims to make the user feedback loop quick and easy.',
     'tech': ['jQuery to keep interactions and animation simple accross browsers', 'SASS variables to build a consistant style for elements', 'Grid system: boostrap'],
-    'images': ['../../img/work/koala.png'],
+    'images': ['../img/work/koala.png'],
     'color': '#f03c8f',
     'shadow1': 'rgba(152, 26, 90, 0.5)',
     'shadow2': 'rgba(152, 26, 90, 0.5)'
@@ -38,7 +38,7 @@ var work = {
     'item-link': 'get-issues',
     'item-description': 'A small command line tool I built to be the fastest way to get Github issues offline. It can be run in any local Github repo, and downloads all open issues, includes markdown syntax. I initially built this help me better understand Node\'s async.waterfall.',
     'tech': ['Uses Node.js as a simple way to make a CLI', 'Github API to get any open issues', 'Additional command line flow if user has 2-factor authentication setup on Github'],
-    'images': ['../../img/work/get-issues.gif'],
+    'images': ['../img/work/get-issues.gif'],
     'color': '#162b3e',
     'shadow1': 'rgba(36, 58, 214, 0.5)',
     'shadow2': 'rgba(44, 32, 234, 0.22)'
@@ -60,7 +60,7 @@ var work = {
     'item-link': 'get-headlines',
     'item-description': 'I started by asking what would change about our relationship to different regions if we could know what is happening. I made this small command line tool that given a city, goes to the different news websites of that region and outputs the latests headlines.',
     'tech': ['Using Node.js to scrape data from different news sites', 'Distributed on npm', 'Used as part of simple local news API'],
-    'images': ['../../img/work/get-headlines.png'],
+    'images': ['../img/work/get-headlines.png'],
     'color': '#ff5343',
     'shadow1': 'rgba(214, 36, 36, 0.5)',
     'shadow2': 'rgba(234, 32, 32, 0.22)'
@@ -71,7 +71,7 @@ var work = {
     'item-link': 'drive-dashboard',
     'item-description': 'I wanted to explore some car data a friend had given me as a quick interface to understand a route.',
     'tech': ['Uses d3.js for animating line charts', 'Uses Leaflet.js for mapping and driving animation'],
-    'images': ['../../img/work/drive.png'],
+    'images': ['../img/work/drive.png'],
     'color': '#99da00',
     'shadow1': 'rgba(80, 214, 36, 0.5)',
     'shadow2': 'rgba(100, 234, 32, 0.22)'
@@ -82,7 +82,7 @@ var work = {
     'item-link': 'ghana-cup',
     'item-description': 'I had a collection of photos and audio from when I covered the world cup events in Toronto. Using popcorn.js I was able to create a unique story that captures the happy choas of the Ghanese community in Toronto during the World Cup.',
     'tech': ['Used Popcorn.js to cue audio and images by frame'],
-    'images': ['../../img/work/ghana.png'],
+    'images': ['../img/work/ghana.png'],
     'color': '#00bf60',
     'shadow1': 'rgba(57, 152, 26, 0.5)',
     'shadow2': 'rgba(100, 234, 32, 0.22)'
